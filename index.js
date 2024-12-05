b1fdf621fda48782b0d6e397430d676e12f4067f
Nathália Rebouças <nathreboucas5@gmail.com>
2024-12-05 13:37:05 -0300

    criando e adicionando as rotas da aplicação

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // configurações
-
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
@@ -7,10 +6,20 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
 
+// template engine
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+// importação de rotas
+const notesRoutes = require('./routes/notes');
+
 // rotas
 app.get('/', function(req, res) {
-    res.send('Hello world')
+    res.render('home');
 });
+
+app.use('/notes', notesRoutes);
 
 app.listen(port, () => {
     console.log(`Projeto rodando na porta: ${port}`)
