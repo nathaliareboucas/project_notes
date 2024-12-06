944c8ab0e555f3eea6a90ec1a1722b9824190f14
Nathália Rebouças <nathreboucas5@gmail.com>
2024-12-05 21:39:45 -0300

    adição de nota

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,23 @@
 const Router = require('express').Router;
+const db = require('../db/connection');
+const { ObjectId } = require('mongodb');
 
 const router = Router();
 
+// form de criação de nota
 router.get('/', function(req, res) {
     res.render('notes/create');
+})
+
+// adição de nota
+router.post('/', function(req, res) {
+    const note = req.body;
+    db.getDb()
+        .db()
+        .collection('notes')
+        .insertOne(note)
+
+    res.redirect(301, '/');
 })
 
 module.exports = router;
