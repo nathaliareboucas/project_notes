0b467c96915e8ba7176623b2568139fb32365b97
Nathália Rebouças <nathreboucas5@gmail.com>
2024-12-06 00:22:04 -0300

    edição da nota

--- routes/notes.js
+++ routes/notes.js
@@ -4,12 +4,12 @@ const { ObjectId } = require('mongodb');
 
 const router = Router();
 
-// form de criação de nota
+// form de criação da nota
 router.get('/', function(req, res) {
     res.render('notes/create');
 })
 
-// adição de nota
+// adição da nota
 router.post('/', function(req, res) {
     const note = req.body;
     db.getDb()
@@ -20,7 +20,7 @@ router.post('/', function(req, res) {
     res.redirect(301, '/');
 })
 
-// remoção de nota
+// remoção da nota
 router.post('/delete', function(req, res) {
     const data = req.body;
     const noteId = ObjectId.createFromHexString(data.id);
@@ -41,6 +41,29 @@ router.get('/:id', async function(req, res) {
         .findOne({_id: noteId})
 
     res.render('notes/details', {note})
+})
+
+// form de edição da nota
+router.get('/edit/:id', async function(req, res) {
+    const noteId = ObjectId.createFromHexString(req.params.id);
+    const note = await db.getDb()
+        .db()
+        .collection('notes')
+        .findOne({_id: noteId})
+
+    res.render('notes/edit', {note})
+})
+
+// edição da nota
+router.post('/update', function(req, res) {
+    const data = req.body;
+    const noteId = ObjectId.createFromHexString(data.id)
+    db.getDb()
+        .db()
+        .collection('notes')
+        .updateOne({_id: noteId}, {$set: {title: data.title, description: data.description}})
+
+    res.redirect('/')
 })
 
 module.exports = router;
